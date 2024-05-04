60acf7772b0af23dafdb6e13e73029766011e86f
Sainul Abid <sainulabidofficial@gmail.com>
2024-05-04 17:39:49 +0530

    feat: password show hide icon added

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,2 +1,6 @@
 export type InputTypes = 'text' | 'number' | 'password' | 'email'
 export type ButtonTypes = 'button' | 'submit'
+
+export type PwdIconProps = React.SVGProps<SVGSVGElement> & {
+  onclick?: () => void
+}
