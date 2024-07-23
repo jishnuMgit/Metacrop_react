6092585b76720bbae73e1791d44e01b59136f87f
abidta <sainulabidofficial@gmail.com>
2024-07-23 12:24:37 +0530

    patch: endofline to lf

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,7 +27,7 @@ import { colors } from '@material-tailwind/react/types/generic'
 
 function Home() {
   return (
-    <div className="mt-12">
+    <div className="mt-12 mx-6">
       <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
         {statisticsCardsData.map(
           ({ icon, title, footerProps, color, ...rest }) => (
